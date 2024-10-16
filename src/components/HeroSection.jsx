90534f78555b8dad74bd7531f931bff13b109af7
Rohit Kotian <rohitkotian07@gmail.com>
2024-10-16 12:45:43 +0530

    Additional CSS Fixes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,10 +4,10 @@ import LoadingBar from "./LoadingBar";
 export default function HeroSection({ loading, imageUrl }) {
   return (
     <>
-      <div className="text-bold text-4xl">
+      <div className="text-bold text-3xl">
         AI Image <span className="text-cyan-400">Generator</span>
       </div>
-      <div className="max-w-sm mt-10 mb-2">
+      <div className="max-w-xs mt-10 mb-2">
         <img className="mb-3" src={imageUrl || defaultImage} alt="" />
         <LoadingBar loading={loading} />
       </div>
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -3,16 +3,16 @@ export default function InputForm({ loading, inputRef, imageGenerator }) {
   return (
     <form
       onSubmit={(e) => imageGenerator(e)}
-      className="flex flex-row w-full md:w-2/5 bg-slate-500 rounded-full h-14 p-1 mb-4"
+      className="flex flex-row w-full md:w-2/5 bg-slate-500 rounded-full h-14 mb-4"
     >
       <input
         ref={inputRef}
-        className="w-full bg-transparent focus:outline-none px-4 placeholder:text-white"
+        className="w-full bg-transparent focus:outline-none px-4 placeholder:text-white placeholder:text-sm"
         placeholder="Describe the image you have in mind..."
         required
       />
       <button
-        className={`rounded-full bg-cyan-500 px-8 font-semibold text-slate-100 ${cursorStatus}`}
+        className={`rounded-full bg-cyan-500 px-8 font-semibold text-teal-50 ${cursorStatus}`}
       >
         Generate
       </button>
